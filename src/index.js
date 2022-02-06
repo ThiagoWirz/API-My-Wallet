ce08b40ffa9dd0e68635745539f019b17729ad42
Thiago <thiagowirzdemoura@gmail.com>
2022-02-06 00:59:04 -0300

    Connection with database

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import { MongoClient, ObjectId } from "mongodb";
 import joi from "joi";
 import { stripHtml } from "string-strip-html";
-dotenv.config();
+
 
 const app = express();
 app.use(cors());
