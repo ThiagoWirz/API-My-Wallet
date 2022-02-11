dd7b7290c98e41b300488a51c2385f2ebbd7382a
leo <celsolnv27@gmail.com>
2022-02-11 11:45:54 -0300

    Preparando revisão de código

--- src/routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { logIn, signUp } from "../controllers/authController.js";
-import { loginValidationMiddleware } from "../middlewares/loginValidationMiddleware.js";
-import { userValidationMiddleware } from "../middlewares/userValidationMiddleware.js";
-
-const authRouter = Router();
-authRouter.post("/auth/sign-up", userValidationMiddleware, signUp);
-authRouter.post("/auth/login", loginValidationMiddleware, logIn);
-export default authRouter;
--- src/schemas/loginSchema.js
+++
@@ -1,8 +0,0 @@
-import joi from "joi";
-
-const logInSchema = joi.object({
-  email: joi.string().required(),
-  password: joi.string().required(),
-});
-
-export default logInSchema;
